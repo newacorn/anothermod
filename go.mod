7cb11799ced73d25cf728e8351dc490b74a72e42
newacorn <w1013d27@126.com>
2025-02-28 19:02:31 +0800

    add HelloWorldOne func.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/newacorn/anothermod
 
 go 1.24
+
+require github.com/newacorn/anotheronemod v0.0.1
--- helloworld.go
+++ helloworld.go
@@ -1,5 +1,10 @@
 package anothermod
 
+import "github.com/newacorn/anotheronemod"
+
 func HelloWorld() {
 	println("Hello World!")
+}
+func HelloWorldOne() {
+	anotheronemod.Dog()
 }
